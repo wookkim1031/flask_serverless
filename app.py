9b4ba0ebeece772ba0732fdb07a1c1c5cf2f0bbb
Ki Wook Kim <wook.kim@rwth-aachen.de>
2025-01-28 10:29:05 +0100

    speeling

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open("config.json", "r") as file:
 
 app = Flask(__name__)
 
-CORS(app, orgins=["https://intern-project-liart.vercel.app"])
+CORS(app, origins=["https://intern-project-liart.vercel.app"])
 
 Sales_url = "https://ncetrlusncjytfjv.public.blob.vercel-storage.com/sales-85mWvldnLkYoqYNPhb8fe6rBpFhqgu.csv"
 Orders_url = "https://ncetrlusncjytfjv.public.blob.vercel-storage.com/orders-uE4hcFvq1xHnzg4tHb8b6qA6rrQ5Z8.csv"
